5f38dd2e2f7dc437d23b8771cb01ef0596a3964c
Narinder Rana <rana.narin@gmail.com>
2023-12-31 07:45:10 +0530

    Annotations for entity class to make setup of database Table

--- src/main/java/org/example/springdatajpa_poc/models/Product.java
+++ src/main/java/org/example/springdatajpa_poc/models/Product.java
@@ -1,17 +1,31 @@
 package org.example.springdatajpa_poc.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(
+        name ="products",
+        schema = "ecommerce",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "sku_unique",
+                        columnNames = "stock_keeping_unit"
+                )
+        }
+)
+
+
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "stock_keeping_unit", nullable = false)
     private String sku;
+    @Column(nullable = false)
     private String name;
     private String description;
     private BigDecimal price;
